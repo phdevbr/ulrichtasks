96b67732180c9f2cf96be24f2c819fea58d676c5
phdevbr <pedroheringer2020@gmail.com>
2023-04-18 15:28:09 -0300

    id is now an uuid

--- db/migrate/20230418180237_create_tasks.rb
+++ db/migrate/20230418180237_create_tasks.rb
@@ -1,11 +1,10 @@
 class CreateTasks < ActiveRecord::Migration[7.0]
   def change
-    create_table :tasks do |t|
+    create_table :tasks, id: :uuid do |t|
       t.string :title
       t.text :description
       t.boolean :checked
-      t.references :user, null: false, foreign_key: true
-
+      t.references :user, type: :uuid, null: false, foreign_key: true
       t.timestamps
     end
   end
