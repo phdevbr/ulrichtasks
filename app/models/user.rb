6f018fa5329dcf749a9e883f727f7e16cda7013a
phdevbr <pedroheringer2020@gmail.com>
2023-04-20 17:01:33 -0300

    username and email are now unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
