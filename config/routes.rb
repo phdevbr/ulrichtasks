3ef604af589211f17780ead0e33da75be7006a7f
phdevbr <pedroheringer2020@gmail.com>
2023-04-20 17:00:24 -0300

    changed login routes to new auth controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  get 'login', to: 'auth#new'
+  post 'login', to: 'auth#create'
+  root 'tasks#index'
 end
