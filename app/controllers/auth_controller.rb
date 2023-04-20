7b2449b7534d5e219c20d775cbb9ede5480b847b
phdevbr <pedroheringer2020@gmail.com>
2023-04-20 17:23:59 -0300

    login is working now

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,13 +3,12 @@ class AuthController < ApplicationController
 
   def create
     user = User.find_by(username: params[:username])
-    redirect_to root_path, notice: 'email: ' + params[:username] + 'user_id:' + user.id
-    # if user && user.password == params[:password]
-    #   cookies[:user_id] = user.id
-    #   redirect_to root_path, notice: 'Login efetuado com sucesso!'
-    # else
-    #   flash.now[:alert] = 'E-mail ou senha inválidos'
-    #   render :new
-    # end
+    if user && user.password == params[:password]
+      cookies[:user_id] = user.id
+      redirect_to root_path, notice: 'Login efetuado com sucesso!'
+    else
+      flash.now[:alert] = 'E-mail ou senha inválidos'
+      render :new
+    end
   end
 end
